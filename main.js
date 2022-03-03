8644be89eb973e222f89d1063367888fc95a3d2e
vionaosiako <viona.osiako@student.morigaschool.com>
2022-03-03 13:27:47 +0300

    This will add arrays in js

---
+++ main.js
@@ -0,0 +1,12 @@
+// function callCalendar(){
+//     document.getElementById("calendar").innerHTML = <a class="nav-link" href="#">Calendar</a>;
+// }
+// Array for the months of the year
+let month = new Array("January","February","March","April","May","June","July","August","September","Octombr","November","Decmber");
+
+//Array for days of the week
+let weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+function dayBorn(){
+
+}
